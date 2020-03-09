5989cb63438effff358110b34a5e2b84b02bf7cd
dibyenduonline <dibyendu.das2@outlook.com>
2020-03-08 23:10:34 -0400

    Add files via upload

---
+++ Function1.cs
@@ -0,0 +1,43 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=dibsstore01;AccountKey=LniZz0s2RaURg3UYak+jhuz2M347eacog61NphdNKn17I9SBIq72P/oeMFtQrefxKCWIxIo2j+DvIVeZXHLi+g==;EndpointSuffix=core.windows.net");
+
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("dibscontainer01");
+
+
+            SharedAccessBlobPolicy adHocPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List
+            };
+
+            string sasContainerToken = cloudBlobContainer.GetSharedAccessSignature(adHocPolicy, null);
+
+            ////
+            ///
+
+            var keyVaultEndpoint = GetKeyVaultEndpoint();
+            var client = new SecretClient(new Uri(keyVaultEndpoint), new DefaultAzureCredential());
+
+            client.SetSecret("dibscontainerkey", sasContainerToken);
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+
+        private static string GetKeyVaultEndpoint() => "https://dibsfunckeyvault.vault.azure.net/";
+    }
+}
